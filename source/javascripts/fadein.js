3acbcd6e0c2c93f5ffbf116ac6cf63d6940f12f5
My Kim Bui <mykimbuibui@gmail.com>
2017-05-02 01:27:03 +0200

    baby js added

---
+++ source/javascripts/fadein.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  window.sr = ScrollReveal({ duration: 2000 });
+  sr.reveal('.firsttext', 50);
+  sr.reveal('.secondtext', 100);
+  sr.reveal('.thirdtext', 150);
+  sr.reveal('.fourthtext', 150);
+
+});
